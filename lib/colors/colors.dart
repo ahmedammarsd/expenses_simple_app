c6070111dffb6fd576626b6b9be2376ad0b148ba
ahmedammarsd <ahmed091999287@gmail.com>
2024-06-19 13:06:19 +0200

    finish init project and some updates in card of total balance

---
+++ lib/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color kWhite = const Color(0xffffffff);
+Color kWhiteTwo = const Color(0xffe6e6e6);
+Color kWhiteThree = const Color(0xffcccccc);
+Color kBlack = const Color(0xff000000);
+Color kBlackTwo = const Color(0xffa1a1a1);
+Color kBlackThree = const Color(0xff333333);
